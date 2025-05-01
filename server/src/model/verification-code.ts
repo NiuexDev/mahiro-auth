7a3a46fd0b0b0c15791685a3328462c8965bbdb9
Ceale <93030615+Ceale@users.noreply.github.com>
2025-05-01 20:31:00 +0800

    refactor(model): 重构 verification-code 模型
    - 将 id 字段重命名为 _id，并设置为 UUID 类型
    - 更新 generate 和 verify 函数中的字段引用
    - 优化代码结构，提高可读性和一致性

--- server/src/model/verification-code.ts
+++ server/src/model/verification-code.ts
@@ -4,7 +4,7 @@ import { Model, model, Schema } from "mongoose"
 import { vcodeCharList as char, vcodeLength } from "~/type/validator/vcode"
 
 export const vcodeSchema = new Schema({
-    id: { type: UUID, required: true, unique: true, index: true },
+    _id: UUID,
     key: { type: String, required: true },
     code: { type: String, required: true },
     expires: { type: Date, required: true, expires: 0 }
@@ -19,7 +19,7 @@ export const vcodeModel = model("vcode", vcodeSchema, "vcode")
 export const generate = async (key: string, length: number, expires: number) => {
     const code = Array.from({ length: length }, () => char[Math.floor(Math.random() * char.length)]).join("")
     return (await vcodeModel.create({
-        id: randomUUID(),
+        _id: randomUUID(),
         key,
         code,
         expires: Date.now() + expires * 1000
@@ -27,7 +27,7 @@ export const generate = async (key: string, length: number, expires: number) => 
 }
 
 export const verify = async (id: string, key: string, code: string) => {
-    const document = await vcodeModel.findOne({ id: Buffer.from(id, "hex") })
+    const document = await vcodeModel.findOne({ _id: Buffer.from(id, "hex") })
     if (document === null) return false
     return document?.key === key && document?.code === code && document?.expires.getTime() > Date.now()
 }

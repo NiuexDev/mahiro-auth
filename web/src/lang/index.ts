27f6ab2361fe5750870ca1aca2aa56506f6ba6e1
Ceale <93030615+Ceale@users.noreply.github.com>
2025-05-10 19:45:16 +0800

    feat(i18n): 优化国际化文案并添加语言名称字段
    - 在 en.json 和 zh-cn.json 中添加 "name" 字段，分别用于英文和中文的语言名称
    - 重构 lang/index.ts，创建 lang 对象和 langList 数组，以便更方便地管理和使用语言资源
    - 更新 i18n 配置，使用新的 lang 对象替代原有的 messages 配置

--- web/src/lang/index.ts
+++ web/src/lang/index.ts
@@ -2,13 +2,17 @@ import { createI18n } from "vue-i18n"
 import zhCN from "./zh-cn.json" assert { type: "json" }
 import en from "./en.json" assert { type: "json" }
 
+export const lang = {
+    "zh-cn": zhCN,
+    "en": en,
+}
+
+export const langList = (Object.keys(lang) as (keyof typeof lang)[]).map(key => ({ langKey: key, langName: lang[key].name }))
+
 const i18n = createI18n({
     locale: localStorage.getItem("lang") || navigator.language?.toLocaleLowerCase(),
     fallbackLocale: "zh-cn",
-    messages: {
-        "zh-cn": zhCN,
-        "en": en,
-    },
+    messages: lang,
     legacy: false,
 })
 
